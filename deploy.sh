0dba296d2f0e809a7359d4ca1cd3dc47dee64079
Harold Booth <harold.booth@nist.gov>
2020-10-09 18:08:05 -0400

    removed references to initial branch name

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,14 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+git init -b main
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:hbooth/en.git master:gh-pages
+git push -f git@github.com:hbooth/en.git main:gh-pages
 
 cd -
