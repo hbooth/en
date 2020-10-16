052d0a1042ae54256263cb2dd4a92eccabde2c00
Harold Booth <harold.booth@nist.gov>
2020-10-16 16:37:32 -0400

    version of git doesn't yet support git init -b switch

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ cd dist
 
 touch .nojekyll
 
-git init -b main
+git init
 git add -A
 git commit -m 'deploy'
 
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:hbooth/en.git main:gh-pages
+git push -f git@github.com:hbooth/en.git master:gh-pages
 
 cd -
